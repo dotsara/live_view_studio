932971d350e48d89615ea0cb9eed64b16a794529
Sara Flemming <dotsara@sent.com>
2020-06-24 15:06:14 -0700

    first pass at search (no results doesn't work, yet)

---
+++ lib/live_view_studio_web/live/search_live.ex
@@ -0,0 +1,128 @@
+defmodule LiveViewStudioWeb.SearchLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Stores
+
+  def mount(_params, _session, socket) do
+    socket =
+      assign(socket,
+        zip: "",
+        #stores: Stores.list_stores()
+        stores: [],
+        loading: false
+      )
+
+      {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+      <h1>Find a Store</h1>
+      <div id="search">
+        <form phx-submit="zip-search">
+          <input type="text" name="zip" value="<%= @zip %>"
+                  placeholder="Zip code"
+                  autofocus
+                  autocomplete="off"
+                  <%= if @loading, do: "readonly" %>/>
+
+          <button type="submit">
+            <img src="images/search.svg">
+          </button>
+        </form>
+
+        <%= if @loading do %>
+        <div class="loader">
+          Loading…
+        </div>
+        <% end %>
+
+        <div class="stores">
+          <ul>
+            <%= for store <- @stores do %>
+              <li>
+                <div class="first-line">
+                  <div class="name">
+                    <%= store.name %>
+                  </div>
+                  <div class="status">
+                    <%= if store.open do %>
+                      <span class="open">Open</span>
+                    <% else %>
+                      <span class="closed">Closed</span>
+                    <% end %>
+                  </div>
+                </div>
+                <div class="second-line">
+                  <div class="street">
+                    <img src="images/location.svg">
+                    <%= store.street %>
+                  </div>
+                  <div class="phone_number">
+                    <img src="images/phone.svg">
+                    <%= store.phone_number %>
+                  </div>
+                </div>
+              </li>
+            <% end %>
+          </ul>
+        </div>
+      </div>
+    """
+  end
+
+  # written this way, the loading spinner (well, bubbles) show up after
+  # the 2-second delay which isn't what we want. The thing is: the
+  # assigns happen in order. So… 
+  # def handle_event("zip-search", %{"zip" => zip}, socket) do
+  #   socket =
+  #     assign(socket,
+  #       zip: zip,
+  #       stores: Stores.search_by_zip(zip)
+  #       loading: true
+  #     )
+  #     {:noreply, socket}
+  # end
+
+  # what we'll do is assign stores an empty list and then we want the
+  # search to run asynchronously, we'll send ourselves an internal message
+  def handle_event("zip-search", %{"zip" => zip}, socket) do
+    send(self(), {:run_zip_search, zip})
+
+    socket =
+      assign(socket,
+        zip: zip,
+        stores: [],
+        loading: true
+      )
+
+      {:noreply, socket}
+  end
+
+  # this is the handl_info module before re-writing it to account for
+  # zero search results.
+  # def handle_info({:run_zip_search, zip}, socket) do
+  #   socket =
+  #     assign(socket,
+  #       stores: Stores.search_by_zip(zip),
+  #       loading: false
+  #     )
+  #     {:noreply, socket}
+  # end
+
+  def handle_event({:run_zip_zearch, zip}, socket) do
+    case Stores.search_by_zip(zip) do
+      [] ->
+        socket =
+          socket
+          |> put_flash(:info, "No stores matching \"#{zip}\"")
+          |> assign(stores: [], loading: false)
+
+        {:noreply, socket}
+
+      stores ->
+        socket = assign(socket, stores: stores, loading: false)
+        {:noreply, socket}
+    end
+  end
+end
